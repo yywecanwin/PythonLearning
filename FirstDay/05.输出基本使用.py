d0bb24c4e09ebf9af0b377534b69e860382dbe2c
32165 <3216561314@qq.com>
2019-09-26 15:16:19 +0800

    完成注释、定义变量及其使用，变量的数据类型，查看关键字列表，输出基本是使用，字符串格式化操作符

---
+++ FirstDay/05.输出基本使用.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# author：yaoyao time:2019/9/26
+
+print("hello pyhton")
+
+print(10)
+
+print(True)
+
+str1 = "爽儿"
+print(str1)
+
+str2 = "尼古拉斯.赵四"
+print(str2)
+print(10 + 20)
