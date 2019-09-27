9e84656f8cd2324e7affbdb30e489bdf6f1fa218
32165 <3216561314@qq.com>
2019-09-28 00:19:49 +0800

    完成转义字符串

---
+++ ThirdDay/12.转义字符.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# author：yaoyao time:2019/9/28
+
+"""
+\n:换行符,打印这个符号,就回来到下一行
+\t:制表符,表示4个字符的宽度
+\\:目录分割符 E:\\工作\\pythonProject
+
+"""
+print("\tyao")
+print("hello\tyao\tah\t")
