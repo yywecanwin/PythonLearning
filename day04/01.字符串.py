0475dcb7f7d9177ccd346bd30699cd8342b2671e
32165 <3216561314@qq.com>
2019-09-28 10:41:51 +0800

    字符串，注释了f_strings用法
    Signed-off-by: 32165 <3216561314@qq.com>

--- day04/01.字符串.py
+++ day04/01.字符串.py
@@ -6,6 +6,11 @@ age = 18
 print("姓名是%s，年龄%d" % (name,age))
 
 # f-strings
+"""
+f-strings 
+提供一种简洁易读的方式, 可以在字符串中包含 Python 表达式. 
+f-strings 以字母 'f' 或 'F' 为前缀, 格式化字符串使用一对单引号、双引号、三单引号、三双引号. 格式化字符串中
+"""
 f_string1 = f"姓名是{name},年龄是{age}"
 print(f_string1)
 
