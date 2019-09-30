6d1a1c70ee72f8c6a544e186dabd04611861dcbf
32165 <3216561314@qq.com>
2019-10-01 00:20:22 +0800

    列表的定义格式和基本使用
    Signed-off-by: 32165 <3216561314@qq.com>

---
+++ day04/10.列表的定义格式和基本使用.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# author：yaoyao time:2019/10/1
+"""
+列表：
+    也是一个容器，可以存储多个元素，元素可以是任何类型的
+定义格式：
+    #非空
+    列表名 = [元素1，元素2，。。。。]
+    空列表
+    列表名 = []
+"""
+name_list = ["yao",'is','a','good','man']
+print(name_list) # ['yao', 'is', 'a', 'good', 'man']
+print(type(name_list)) # <class 'list'>
+
+
+list1 = [10, 3.14, "hello", True]
+print(list1) # [10, 3.14, 'hello', True]
+
+#定义空列表
+list2 = []
+print(list2) # []
+
