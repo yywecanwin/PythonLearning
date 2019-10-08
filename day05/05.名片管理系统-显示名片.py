d1c923ee8da4e5ba493ac0193fb4f61fdbe05f75
32165 <3216561314@qq.com>
2019-10-08 10:56:09 +0800

    新建一个字段
    Signed-off-by: 32165 <3216561314@qq.com>

--- day05/05.名片管理系统-显示名片.py
+++ day05/05.名片管理系统-显示名片.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 # author：yaoyao time:2019/10/8
 
-
-
-
+card_list = [
+            {'company': '阿里巴巴集团', 'name': 'Jack.Ma', 'title': 'CEO', 'tel': '18888888888',
+              'email': 'jack@alibaba.com', 'address': '杭州市阿里科技园1号楼9层'},
+            {'company': '阿里巴巴集团', 'name': '张勇', 'title': '执行CEO', 'tel': '17777777777',
+              'email': 'zhangyong@alibaba.com', 'address': '杭州市阿里科技园1号楼9层'}
+            ]
 
