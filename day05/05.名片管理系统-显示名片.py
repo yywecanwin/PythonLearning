2cd47ef6a9fb3b173fd46784bd7fce9ef37ccd8a
32165 <3216561314@qq.com>
2019-12-18 16:17:08 +0800

    显示名片
    Signed-off-by: 32165 <3216561314@qq.com>

--- day05/05.名片管理系统-显示名片.py
+++ day05/05.名片管理系统-显示名片.py
@@ -8,3 +8,39 @@ card_list = [
               'email': 'zhangyong@alibaba.com', 'address': '杭州市阿里科技园1号楼9层'}
             ]
 
+
+# 1、遍历列表得到 名片（字典）
+
+for card in card_list:
+    # 2把名片显示出来
+
+    company = card.get("company")
+    name = card.get("name")
+    title = card.get("title")
+    tel = card.get("tel")
+    email = card.get("email")
+    address = card.get("address")
+
+    # 2.1从字典中取出公司名称，姓名、职位、手机邮箱
+    print(company,end="")
+    print(name.rjust(10),end="")
+    print("职位：" + title,end="")
+    print("邮箱：" + email,end="")
+    print("地址：" + address)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
