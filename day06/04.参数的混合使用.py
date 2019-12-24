20d188f2728e2ce1c868f0b20433363a46fc1eca
32165 <3216561314@qq.com>
2019-12-24 09:14:25 +0800

    修改04.参数的混合使用，备注
    Signed-off-by: 32165 <3216561314@qq.com>

--- day06/04.参数的混合使用.py
+++ day06/04.参数的混合使用.py
@@ -2,10 +2,10 @@
 # author：yaoyao time:2019/12/20
 
 def sum_nums_3(a,*args,b=22,c=33,**kwargs):
-    print(a)
-    print(b)
-    print(c)
-    print(args)
-    print(kwargs)
+    print(a) # 100
+    print(b) # 1
+    print(c) # 2
+    print(args) # (200, 300, 400, 500)
+    print(kwargs) # {'d': 3, 'e': 6}
 
 sum_nums_3(100,200,300,400,500,b=1,c=2,d=3,e=6)
