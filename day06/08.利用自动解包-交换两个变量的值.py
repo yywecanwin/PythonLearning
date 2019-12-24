578958119812ed59b25ab65fe1635e1ba770e065
32165 <3216561314@qq.com>
2019-12-24 09:35:40 +0800

    完成利用自动解析包，实现交换两个变量的值
    Signed-off-by: 32165 <3216561314@qq.com>

---
+++ day06/08.利用自动解包-交换两个变量的值.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# author：yaoyao time:2019/12/24
+
+a = 10
+b = 20
+
+# 1、借助一个中间变量
+# temp = a
+# a = b
+# b = temp
+# print(a) # 20
+# print(b) # 10
+
+# 2.借助两个变量的和
+# c = a + b
+# b = c - a
+# a = c - c - a
+# c - a
+# a = c - a
+# b = c - b
+# print(a)
+# print(b)
+
+#3.利用自动解包
+
+a,b = b,a
+
+print(a) # 20
+print(b) # 10
+
+
+
+
+
+
+
+
