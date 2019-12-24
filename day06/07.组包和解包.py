284dade17d700c58770c6b5a22e199d259fe5c5e
32165 <3216561314@qq.com>
2019-12-24 09:28:23 +0800

    完成了组包和解包
    Signed-off-by: 32165 <3216561314@qq.com>

---
+++ day06/07.组包和解包.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# author：yaoyao time:2019/12/24
+
+# 自动组包
+tuple1 = 10,20
+print(tuple1) #(10, 20)
+
+# 自动解包
+# 要求左边的变量个数与元组中元素的个数相同
+a,b = (10,20)
+print(a) # 10
+print(b) # 20
+
+# 对列表自动解包
+c,d = [10,20]
+print(c) # 10
+print(d) # 20
+
+
+
+
+
