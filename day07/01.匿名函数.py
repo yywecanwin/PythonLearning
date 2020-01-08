1bc455d88da584259b46c184368c309817163fa9
32165 <3216561314@qq.com>
2020-01-08 21:47:40 +0800

    完成匿名函数
    Signed-off-by: 32165 <3216561314@qq.com>

---
+++ day07/01.匿名函数.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# author：yaoyao time:2020/1/8
+
+"""
+def my_add(a,b):
+    return a + b
+
+print(my_add(10,20))
+
+"""
+
+# lambda 形参1，形参2，.....:单行表达式 或者 调用其他函数的代数
+my_add = lambda a,b : a + b
+
+sum = my_add(1,2)
+print(sum)
+
