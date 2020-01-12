7f36cbc7d188ace6e7c63cf8475a2e0ef90b7c04
32165 <3216561314@qq.com>
2020-01-13 00:14:49 +0800

    完成，reduce用法
    Signed-off-by: 32165 <3216561314@qq.com>

--- day07/07.高阶函数.py
+++ day07/07.高阶函数.py
@@ -21,14 +21,35 @@ print(type(result),result,list(result)) # <class 'map'> <map object at 0x0000022
 
 """首字母大写"""
 
-my_list = ['smith', 'edward', 'john', 'obama', 'tom']
+my_list1 = ['smith', 'edward', 'john', 'obama', 'tom']
 
 def f1(x):
     return x[0].upper() + x[1:]
 
-result1 = map(f1,my_list)
+result1 = map(f1,my_list1)
 
-print(list(result1))
+print(list(result1)) # ['Smith', 'Edward', 'John', 'Obama', 'Tom']
+
+"""
+reduce用法
+reduce() 函数会对参数序列中元素进行累计.
+
+函数将一个数据集合中的所有数据进行下列操作:
+    1、用传给 reduce 中的函数 function（有两个参数）先对集合中的第 1、2 个元素进行操作.
+    2、得到的结果再与第三个数据用 function 函数运算, 最后得到一个结果.    
+"""
+
+"""计算列表中的累加和"""
+
+my_list2 = [1,2,3,4,5]
+
+def f2(x1,x2):
+
+    return x1 + x2
+
+result2 = functools.reduce(f2,my_list2)
+print(result2) # 15
+
 
 
 
