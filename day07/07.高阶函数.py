07729bad7cabc905f7786640cd91d89935102fea
32165 <3216561314@qq.com>
2020-01-13 00:23:16 +0800

    完成，filter用法
    Signed-off-by: 32165 <3216561314@qq.com>

--- day07/07.高阶函数.py
+++ day07/07.高阶函数.py
@@ -51,6 +51,28 @@ result2 = functools.reduce(f2,my_list2)
 print(result2) # 15
 
 
+"""
+filter
+
+filter() 函数用于过滤序列, 过滤掉不符合条件的元素, 返回一个 filter 对象, 如果要转换为列表, 可以使用 list() 来转换.
+
+该接收两个参数, 第一个为函数, 第二个为序列, 序列的每个元素作为参数传递给函数进行判, 然后返回 True 或 False, 最后将返回 True 的元素放到新列表中
+
+"""
+
+"""过滤列表中的偶数"""
+
+my_list3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+def f3(x):
+    return x % 2 == 0
+
+result3 = filter(f3,my_list3)
+print(result3)
+print(list(result3))
+
+
+
 
 
 
