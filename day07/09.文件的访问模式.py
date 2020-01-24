429bf0f6622158aec9a163ff271a5666623b9a3e
32165 <3216561314@qq.com>
2020-01-24 23:19:08 +0800

    完成,文件的访问模式
    Signed-off-by: 32165 <3216561314@qq.com>

---
+++ day07/09.文件的访问模式.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# author：yaoyao time:2020/1/24
+
+f = open("a.txt","r")
+# print(f.read(4))
+# print(f.read(4))
+# print(f.read(4))
+
+f = open("a.txt","wb")
+f.write(b"Android")
+
+f.close()
+
+
+
+
+
