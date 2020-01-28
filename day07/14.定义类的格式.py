443ba8175d6d9628eb78dc391e2bd2b8a9d05a53
32165 <3216561314@qq.com>
2020-01-28 19:29:29 +0800

    完成,定义类的格式
    Signed-off-by: 32165 <3216561314@qq.com>

---
+++ day07/14.定义类的格式.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# author：yaoyao time:2020/1/28
+
+"""
+1.定义类的格式
+    class 类名：
+        方法列表
+
+2.定义方法的格式
+    def 方法名（self,形参2，形参3，。。。）
+        方法体
+
+"""
+
+class Student:
+
+    # 吃饭
+    def eat(self,food):
+        print(f"吃{food}")
+
+    # 睡觉
+    def sleep(self,where):
+        print(f"睡在{where}")
+
+    # 学习
+    def study(self,course):
+        print(f"学习{course}")
+        print("为了美好的未来加油把")
+
+    pass
