96ec24867700e98cfb66d11d2d05fe55d6b07154
32165 <3216561314@qq.com>
2020-02-01 19:52:35 +0800

    完成，object类
    Signed-off-by: 32165 <3216561314@qq.com>

---
+++ day09/02.object类.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# author：yaoyao time:2020/2/1
+
+"""
+object类：
+    是所有类的父类，任何一个默认都是继承object类
+
+
+
+"""
+
+
+
+object = object()
+
+# class Student:
+#
+#     pass
+
+# 等价于下面的写法
+class Student(object):
+    pass
+
+s = Student()
+
+
+
+
+
+
