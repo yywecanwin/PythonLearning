685d29f7c5b3d35a6fd0f9046402eda27758a70e
32165 <3216561314@qq.com>
2020-02-03 00:57:06 +0800

    完成，类的对象
    Signed-off-by: 32165 <3216561314@qq.com>

---
+++ day09/11.类的对象.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# author：yaoyao time:2020/2/2
+
+"""
+类对象：
+    也是一个对象
+
+    一个类 就是一个  类对象
+    类对象的名字，就是 类的 名字
+
+"""
+
+class Student:
+
+    def __init__(self,name,age,gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        pass
+
+    def study(self):
+        print("哈哈哈。。。。")
+        pass
+
+    pass
+
+
+
+
+
+
+
