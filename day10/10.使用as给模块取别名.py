3b9faea5ac756d192e7dc0026103422660fa7fa5
32165 <3216561314@qq.com>
2020-02-10 11:51:54 +0800

    完成,使用as给模块取别名
    Signed-off-by: 32165 <3216561314@qq.com>

---
+++ day10/10.使用as给模块取别名.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# author：yaoyao time:2020/2/10
+
+import module02 as mo2,module01 as mo1
+
+print(mo1.a)
+print(mo1.func1(10,20))
+s = mo1.Student("Tom",20)
+print(s)
+
+print(mo2.a)
+print(mo2.func1(10,20))
